fad72d3bdb0f6fcf74fad995ea0a952315c8f985
Khei Wong <kwon491@aucklanduni.ac.nz>
2015-06-03 04:35:55 +0000

    Add assignment files to local git

---
+++ kwon491/javascript.js
@@ -0,0 +1,31 @@
+
+
+//6a
+	$(document).ready(function(){
+     $("button.button1").click(function(){
+          $(#logo1).attr('src', 'turnoff.jpg');
+		            $(#logo2).attr('src', 'turnoff.jpg');
+     });
+});
+//6b
+
+	$(document).ready(function(){
+		$("button.button2").click(function(){
+			$("button.button1").css('color', "green");
+			$("button.button2").css('color', "blue");
+			$("button.button3").css('color', "yellow");
+			$("h1").css('font-family', "verdana,sans-serif");			
+		});
+	});
+	
+//6c
+	$(document).ready(function(){
+		$("button.button3").click(function(){
+			$("#mainnav").animate({
+				right: '800px',
+				opacity: '0.75',
+			});
+		});
+	});
+	
+	
